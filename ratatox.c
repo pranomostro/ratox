7a06e015762d5f585b44d5d9e176605786693830
sin <sin@2f30.org>
2014-09-15 11:39:09 +0100

    Capitalize string in output

--- ratatox.c
+++ ratatox.c
@@ -690,7 +690,7 @@ again:
 			if (cmd[1] == '\0' || isspace((int)cmd[1]))
 				return (*cmds[i].cb)(cmd, strlen(cmd));
 
-	fprintf(stderr, "unknown command: %s\n", cmd);
+	fprintf(stderr, "Unknown command: %s\n", cmd);
 	return -1;
 }
 
