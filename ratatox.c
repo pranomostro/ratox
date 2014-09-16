15fc377636689e4f11e170db911abc8b745c0483
sin <sin@2f30.org>
2014-09-16 14:13:25 +0100

    Print message when transfers start

--- ratatox.c
+++ ratatox.c
@@ -972,6 +972,7 @@ loop(void)
 						f->t.state = TRANSFER_INITIATED;
 						tox_new_file_sender(tox, f->fid,
 							0, (uint8_t *)"file", strlen("file") + 1);
+						printout("Initiated transfer...\n");
 						break;
 					case TRANSFER_INPROGRESS:
 						send_friend_file(f);
