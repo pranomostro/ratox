d8013df97d768878472acca26cc67c58b2b33b4f
FRIGN <dev@frign.de>
2014-10-05 20:08:55 +0200

    Fix units (us -> ms)
    Got off a factor of a thousand, this would've never worked.
    Now it should handle the calls properly.

--- ratox.c
+++ ratox.c
@@ -600,8 +600,8 @@ sendfriendcalldata(struct friend *f)
 
 	clock_gettime(CLOCK_MONOTONIC, &now);
 	diff = timediff(f->av.lastsent, now);
-	if (diff.tv_nsec == 0 && diff.tv_nsec < toxavconfig.audio_frame_duration * 1000) {
-		diff.tv_nsec = toxavconfig.audio_frame_duration * 1000 - diff.tv_nsec;
+	if (diff.tv_nsec == 0 && diff.tv_nsec < toxavconfig.audio_frame_duration * 1E6) {
+		diff.tv_nsec = toxavconfig.audio_frame_duration * 1E6 - diff.tv_nsec;
 		nanosleep(&diff, NULL);
 	}
 	f->av.lastsent = now;
