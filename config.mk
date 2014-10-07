4292294e0cc7f68c5272faf4eeff190fd810987c
sin <sin@2f30.org>
2014-10-07 01:49:26 +0100

    Revert accidental modification to config.mk

--- config.mk
+++ config.mk
@@ -8,5 +8,5 @@ MANPREFIX = $(PREFIX)/share/man
 CC = cc
 LD = $(CC)
 CPPFLAGS = -DVERSION=\"${VERSION}\"
-CFLAGS   = -g -I/usr/local/include -Wall -Wunused $(CPPFLAGS)
-LDFLAGS  = -g -L/usr/local/lib -ltoxcore -ltoxav -ltoxencryptsave
+CFLAGS   = -I/usr/local/include -Wall -Wunused $(CPPFLAGS)
+LDFLAGS  = -L/usr/local/lib -ltoxcore -ltoxav -ltoxencryptsave
