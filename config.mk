106a87741426a9d2f470e5295e20de17a17c4822
sin <sin@2f30.org>
2014-10-08 14:41:59 +0100

    Update README

--- config.mk
+++ config.mk
@@ -9,4 +9,4 @@ CC = cc
 LD = $(CC)
 CPPFLAGS = -DVERSION=\"${VERSION}\"
 CFLAGS   = -I/usr/local/include -Wall -Wunused $(CPPFLAGS)
-LDFLAGS  = -L/usr/local/lib -lrt -ltoxcore -ltoxav -ltoxencryptsave
+LDFLAGS  = -L/usr/local/lib -ltoxcore -ltoxav -ltoxencryptsave
