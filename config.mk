0db821b7bd566f6cfdc0cc5a7bbcc3e5e92adb4c
pranomostro <pranomostro@posteo.net>
2017-06-28 14:28:33 +0200

    Bump to 0.4

--- config.mk
+++ config.mk
@@ -1,5 +1,5 @@
 # ratox version
-VERSION = 0.3
+VERSION = 0.4
 
 # paths
 PREFIX = /usr/local
@@ -8,6 +8,6 @@ MANPREFIX = $(PREFIX)/share/man
 CC = cc
 LD = $(CC)
 CPPFLAGS = -DVERSION=\"${VERSION}\"
-CFLAGS   = -g -I/usr/local/include -Wall -Wunused $(CPPFLAGS)
-LDFLAGS  = -g -L/usr/local/lib
+CFLAGS   = -I/usr/local/include -Wall -Wunused $(CPPFLAGS)
+LDFLAGS  = -L/usr/local/lib
 LDLIBS   = -ltoxcore -ltoxav -ltoxencryptsave -lsodium -lopus -lvpx -lm -lpthread
