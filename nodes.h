3fdd7181094d2838ee1210e23da5092ad5864370
pranomostro <pranomostro@posteo.net>
2019-08-21 14:37:43 +0200

    Updated bootstrap nodes.

--- nodes.h
+++ nodes.h
@@ -1,26 +1,5 @@
 static struct node nodes[] = {
 	{
-		.addr4 = "node.tox.biribiri.org",
-		.addr6 = NULL,
-		.udp_port = 33445,
-		.tcp_port = 3389,
-		.idstr = "F404ABAA1C99A9D37D61AB54898F56793E1DEF8BD46B1038B9D822E8460FAB67"
-	},
-	{
-		.addr4 = "130.133.110.14",
-		.addr6 = "2001:6f8:1c3c:babe::14:1",
-		.udp_port = 33445,
-		.tcp_port = 33445,
-		.idstr = "461FA3776EF0FA655F1A05477DF1B3B614F7D6B124F7DB1DD4FE3C08B03B640F"
-	},
-	{
-		.addr4 = "198.98.51.198",
-		.addr6 = "2605:6400:1:fed5:22:45af:ec10:f329",
-		.udp_port = 33445,
-		.tcp_port = 33445,
-		.idstr = "1D5A5F2F5D6233058BF0259B09622FB40B482E4FA0931EB8FD3AB8E7BF7DAF6F"
-	},
-	{
 		.addr4 = "85.172.30.117",
 		.addr6 = NULL,
 		.udp_port = 33445,
@@ -28,59 +7,17 @@ static struct node nodes[] = {
 		.idstr = "8E7D0B859922EF569298B4D261A8CCB5FEA14FB91ED412A7603A585A25698832"
 	},
 	{
-		.addr4 = "194.249.212.109",
-		.addr6 = "2001:1470:fbfe::109",
+		.addr4 = "85.143.221.42",
+		.addr6 = "2a04:ac00:1:9f00:5054:ff:fe01:becd",
 		.udp_port = 33445,
 		.tcp_port = 3389,
-		.idstr = "3CEE1F054081E7A011234883BC4FC39F661A55B73637A5AC293DDF1251D9432B"
-	},
-	{
-		.addr4 = "185.25.116.107",
-		.addr6 = "2a00:7a60:0:746b::3",
-		.udp_port = 33445,
-		.tcp_port = 33445,
 		.idstr = "DA4E4ED4B697F2E9B000EEFE3A34B554ACD3F45F5C96EAEA2516DD7FF9AF7B43"
 	},
 	{
-		.addr4 = "217.182.143.254",
-		.addr6 = "2001:41d0:302:1000::e111",
-		.udp_port = 2306,
-		.tcp_port = 2306,
-		.idstr = "7AED21F94D82B05774F697B209628CD5A9AD17E0C073D9329076A4C28ED28147"
-	},
-	{
-		.addr4 = "104.223.122.15",
-		.addr6 = "2607:ff48:aa81:800::35eb:1",
-		.udp_port = 33445,
-		.tcp_port = 33445,
-		.idstr = "0FB96EEBFB1650DDB52E70CF773DDFCABE25A95CC3BB50FC251082E4B63EF82A"
-	},
-	{
-		.addr4 = "51.254.84.212",
-		.addr6 = "2001:41d0:a:1a3b::18",
+		.addr4 = "163.172.136.118",
+		.addr6 = "2001:bc8:4400:2100::13:41d",
 		.udp_port = 33445,
 		.tcp_port = 3389,
-		.idstr = "AEC204B9A4501412D5F0BB67D9C81B5DB3EE6ADA64122D32A3E9B093D544327D"
-	},
-	{
-		.addr4 = "213.183.51.211",
-		.addr6 = "2a06:f901:1:100::98",
-		.udp_port = 33445,
-		.tcp_port = 33445,
-		.idstr = "B71E91E2F5029B0A84D3B1136319CDD3D1DB6D3702B6CEFA66A4BEB25A635916"
-	},
-	{
-		.addr4 = "toxnode.nek0.net",
-		.addr6 = "toxnode.nek0.net",
-		.udp_port = 33445,
-		.tcp_port = 33445,
-		.idstr = "20965721D32CE50C3E837DD75B33908B33037E6225110BFF209277AEAF3F9639"
-	},
-	{
-		.addr4 = "163.172.136.118",
-		.addr6 = "2001:bc8:4400:2100::1c:50f",
-		.udp_port = 33445,
-		.tcp_port = 33445,
 		.idstr = "2C289F9F37C20D09DA83565588BF496FAB3764853FA38141817A72E3F18ACA0B"
 	},
 	{
@@ -91,39 +28,25 @@ static struct node nodes[] = {
 		.idstr = "02807CF4F8BB8FB390CC3794BDF1E8449E9A8392C5D3F2200019DA9F1E812E46"
 	},
 	{
-		.addr4 = "37.97.185.116",
-		.addr6 = NULL,
-		.udp_port = 33445,
-		.tcp_port = 33445,
-		.idstr = "E59A0E71ADA20D35BD1B0957059D7EF7E7792B3D680AE25C6F4DBBA09114D165"
-	},
-	{
-		.addr4 = "80.87.193.193",
-		.addr6 = "2a01:230:2:6::46a8",
-		.udp_port = 33445,
-		.tcp_port = 3389,
-		.idstr = "B38255EE4B054924F6D79A5E6E5889EC94B6ADF6FE9906F97A3D01E3D083223A"
-	},
-	{
-		.addr4 = "initramfs.io",
+		.addr4 = "tox.initramfs.io",
 		.addr6 = "tox.initramfs.io",
 		.udp_port = 33445,
-		.tcp_port = 33445,
+		.tcp_port = 3389,
 		.idstr = "3F0A45A268367C1BEA652F258C85F4A66DA76BCAA667A49E770BCC4917AB6A25"
 	},
 	{
-		.addr4 = "tox.ngc.zone",
-		.addr6 = "tox.ngc.zone",
+		.addr4 = "tox.neuland.technology",
+		.addr6 = "tox.neuland.technology",
 		.udp_port = 33445,
-		.tcp_port = 33445,
+		.tcp_port = 3389,
 		.idstr = "15E9C309CFCB79FDDF0EBA057DABB49FE15F3803B1BFF06536AE2E5BA5E4690E"
 	},
 	{
-		.addr4 = "136.243.141.187",
-		.addr6 = "2a01:4f8:212:2459::a:1337",
-		.udp_port = 443,
+		.addr4 = "144.217.167.73",
+		.addr6 = NULL,
+		.udp_port = 33445,
 		.tcp_port = 3389,
-		.idstr = "6EE1FADE9F55CC7938234CC07C864081FC606D8FE7B751EDA217F268F1078A39"
+		.idstr = "7E5668E0EE09E19F320AD47902419331FFEE147BB3606769CFBE921A2A2FD34C"
 	},
 	{
 		.addr4 = "tox.abilinski.com",
@@ -131,20 +54,6 @@ static struct node nodes[] = {
 		.udp_port = 33445,
 		.tcp_port = 33445,
 		.idstr = "10C00EB250C3233E343E2AEBA07115A5C28920E9C8D29492F6D00B29049EDC7E"
-	},
-	{
-		.addr4 = "tox-node.loskiq.it",
-		.addr6 = NULL,
-		.udp_port = 33445,
-		.tcp_port = 3389,
-		.idstr = "88124F3C18C6CFA8778B7679B7329A333616BD27A4DFB562D476681315CF143D"
-	},
-	{
-		.addr4 = "tmux.ru",
-		.addr6 = NULL,
-		.udp_port = 33445,
-		.tcp_port = 33445,
-		.idstr = "7467AFA626D3246343170B309BA5BDC975DF3924FC9D7A5917FBFA9F5CD5CD38"
 	},
 	{
 		.addr4 = "37.48.122.22",
@@ -157,22 +66,8 @@ static struct node nodes[] = {
 		.addr4 = "tox.novg.net",
 		.addr6 = NULL,
 		.udp_port = 33445,
-		.tcp_port = 33445,
-		.idstr = "D527E5847F8330D628DAB1814F0A422F6DC9D0A300E6C357634EE2DA88C35463"
-	},
-	{
-		.addr4 = "t0x-node1.weba.ru",
-		.addr6 = NULL,
-		.udp_port = 33445,
 		.tcp_port = 3389,
-		.idstr = "5A59705F86B9FC0671FDF72ED9BB5E55015FF20B349985543DDD4B0656CA1C63"
-	},
-	{
-		.addr4 = "79.140.30.52",
-		.addr6 = NULL,
-		.udp_port = 33445,
-		.tcp_port = 33445,
-		.idstr = "FFAC871E85B1E1487F87AE7C76726AE0E60318A85F6A1669E04C47EB8DC7C72D"
+		.idstr = "D527E5847F8330D628DAB1814F0A422F6DC9D0A300E6C357634EE2DA88C35463"
 	},
 	{
 		.addr4 = "95.31.18.227",
@@ -182,17 +77,52 @@ static struct node nodes[] = {
 		.idstr = "257744DBF57BE3E117FE05D145B5F806089428D4DCE4E3D0D50616AA16D9417E"
 	},
 	{
-		.addr4 = "tox.er0p.org",
-		.addr6 = "fe80::5054:ff:fe75:173",
+		.addr4 = "198.199.98.108",
+		.addr6 = "2604:a880:1:20::32f:1001",
 		.udp_port = 33445,
 		.tcp_port = 3389,
-		.idstr = "FB6A7FFE8F144B3ACBD00B7C644AFA14F8764DFADE6DA5691965C7F45A604450"
+		.idstr = "BEF0CFB37AF874BD17B9A8F9FE64C75521DB95A37D33C5BDB00E9CF58659C04F"
 	},
 	{
-		.addr4 = "46.101.197.175",
-		.addr6 = "2a03:b0c0:3:d0::ac:5001",
-		.udp_port = 443,
+		.addr4 = "52.53.185.100",
+		.addr6 = NULL,
+		.udp_port = 33445,
 		.tcp_port = 33445,
-		.idstr = "CD133B521159541FB1D326DE9850F5E56A6C724B5B8E5EB5CD8D950408E95707"
+		.idstr = "A04F5FE1D006871588C8EC163676458C1EC75B20B4A147433D271E1E85DAF839"
+	},
+	{
+		.addr4 = "tox.kurnevsky.net",
+		.addr6 = "tox.kurnevsky.net",
+		.udp_port = 33445,
+		.tcp_port = 33445,
+		.idstr = "82EF82BA33445A1F91A7DB27189ECFC0C013E06E3DA71F588ED692BED625EC23"
+	},
+	{
+		.addr4 = "116.196.77.132",
+		.addr6 = NULL,
+		.udp_port = 33445,
+		.tcp_port = 3389,
+		.idstr = "040326E850DDCB49B1B2D9E3E2789D425774E4C5D783A55C09A024D05D2A8A66"
+	},
+	{
+		.addr4 = "87.118.126.207",
+		.addr6 = NULL,
+		.udp_port = 33445,
+		.tcp_port = 33445,
+		.idstr = "0D303B1778CA102035DA01334E7B1855A45C3EFBC9A83B9D916FFDEBC6DD3B2E"
+	},
+	{
+		.addr4 = "81.169.136.229",
+		.addr6 = "2a01:238:4254:2a00:7aca:fe8c:68e0:27ec",
+		.udp_port = 33445,
+		.tcp_port = 3389,
+		.idstr = "D031DAC44F00464D3C9636F9850BF0064BC37FEB55789A13B6F59052CAE8A958"
+	},
+	{
+		.addr4 = "205.185.115.131",
+		.addr6 = NULL,
+		.udp_port = 53,
+		.tcp_port = 33445,
+		.idstr = "3091C6BEB2A993F1C6300C16549FABA67098FF3D62C6D253828B531470B53D68"
 	}
 };
